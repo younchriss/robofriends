98632ddae46884127d9828f4506fe5b8f2b4a686
Chris <chris@Christian-Test.local>
2020-08-22 00:08:12 +0100

    Updates

---
+++ precache-manifest.ab0970d781daaff282927a7a3fafed71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fdf12953cd3a1313b47986b7d439d5a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "76ac9981932260ad670a",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "9eb2e2c229a52b22a0d8",
+    "url": "/robofriends/static/css/main.7f5d6dd1.chunk.css"
+  },
+  {
+    "revision": "76ac9981932260ad670a",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eb2e2c229a52b22a0d8",
+    "url": "/robofriends/static/js/main.9a9f7331.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
